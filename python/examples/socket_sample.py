924e0c7c16a3bfadf51180876935115949e6d020
RR294 <rajeshvrathod@gmail.com>
2019-03-24 09:10:03 +0530

    Sample files for socket & urllib.

---
+++ python/examples/socket_sample.py
@@ -0,0 +1,19 @@
+import socket
+
+# create a socket
+mysock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org',80))
+
+# send request
+cmd='GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+#cmd='GET http://google.co.in HTTP/1.0\r\n\r\n'.encode()
+
+mysock.send(cmd)
+
+while True:
+    data=mysock.recv(512)
+    if len(data)<1:
+        break
+    print(data.decode(),end='')
+    
+mysock.close()
